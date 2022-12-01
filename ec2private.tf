a7ad91e6a6c59f68d099defefe55e68b06d3efc0
manojkemmi <manojkm381@gmail.com>
2022-12-01 19:04:16 +0530

    second commit

---
+++ ec2private.tf
@@ -0,0 +1,93 @@
+provider "aws" {
+  region = ""
+  access_key = ""
+  secret_key = ""
+  }
+
+resource "aws_vpc" "my-vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "subnet-private" {
+  vpc_id     = aws_vpc.my-vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_nat_gateway" "NAT-GW" {
+  connectivity_type = "private"
+  subnet_id         = aws_subnet.subnet-private.id
+}
+
+
+resource "aws_route_table" "private-route-table" {
+  vpc_id = aws_vpc.my-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.NAT-GW.id
+  }
+}
+
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.subnet-private.id
+  route_table_id = aws_route_table.private-route-table.id
+}
+
+
+
+resource "aws_security_group" "allow_web" {
+  name        = "allow_web_traffic"
+  description = "Allow web inbound traffic"
+  vpc_id      =aws_vpc.my-vpc.id
+
+  ingress {
+    description      = "HTTPS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+
+ingress {
+    description      = "HTTP from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+
+ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+  
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+resource "aws_eip" "one" {
+  instance = aws_instance.web-server-instance.id
+  vpc      = true
+}
+
+
+
+resource "aws_instance" "web-server-instance" {
+  ami = "ami-0b0dcb5067f052a63"
+  instance_type = "t2.micro"
+  availability_zone = "us-east-1a"
+  key_name = "my-keypair"
+}
